18c47301e28445d879db41b981653fe298dd7c93
Dotun Olusoga <adedotun.olusoga@gmail.com>
2016-01-03 15:11:14 -0600

    minor change to purchase_mailer.rb

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,6 +1,6 @@
 class PurchaseMailer < ActionMailer::Base
   layout 'purchase_mailer'
-  default from: "Tealchest <hello@tealchest.com"
+  default from: "Tealchest <hello@tealchest.com>"
 
   def purchase_receipt purchase
     @purchase = purchase
